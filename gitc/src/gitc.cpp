f6965405ea77caa63185e1d91e035d64302889f4
ZHA Jierui <jierui.zha@insa-rouen.fr>
2017-03-17 10:30:33 +0100

    gd	zvdgbkbvjhe

---
+++ gitc/src/gitc.cpp
@@ -0,0 +1,16 @@
+//============================================================================
+// Name        : gitc.cpp
+// Author      : ZHA Jierui
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	cout << "premier git";
+	return 0;
+}
